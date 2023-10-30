598eae1a5613941b72e9132a6dfb8778d3da44c3
PrimeEagle <misc@hiddendragon.com>
2023-10-30 18:04:17 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/VNet/VNet.System@1494 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

--- VNet.System/Services/TempFileServiceOptions.cs
+++ VNet.System/Services/TempFileServiceOptions.cs
@@ -1,11 +1,8 @@
-﻿namespace VNet.System.Services;
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+namespace VNet.System.Services;
 
 public class TempFileServiceOptions
 {
     public string TempDirectory { get; set; }
-
-    public TempFileServiceOptions(string tempDirectory)
-    {
-        TempDirectory = tempDirectory;
-    }
 }
