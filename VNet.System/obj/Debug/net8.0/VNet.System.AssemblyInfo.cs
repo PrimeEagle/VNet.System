cb7c383257c2ef9c8b5373acb45e10ec7515f2c1
PrimeEagle <misc@hiddendragon.com>
2023-12-04 21:31:33 -0600

    updating for GitHub

--- VNet.System/obj/Debug/net8.0/VNet.System.AssemblyInfo.cs
+++ VNet.System/obj/Debug/net8.0/VNet.System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VNet.System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdbf8d6914b2943937392e3076c5f5aad0a75f35")]
 [assembly: System.Reflection.AssemblyProductAttribute("VNet.System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VNet.System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
