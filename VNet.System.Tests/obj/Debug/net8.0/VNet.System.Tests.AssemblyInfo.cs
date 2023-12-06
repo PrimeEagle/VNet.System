b8c797f46441cfd448e7773cb06aa1d0571dffed
PrimeEagle <misc@hiddendragon.com>
2023-12-05 22:05:18 -0600

    update workflows

--- VNet.System.Tests/obj/Debug/net8.0/VNet.System.Tests.AssemblyInfo.cs
+++ VNet.System.Tests/obj/Debug/net8.0/VNet.System.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VNet.System.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5daca3037cac900da1f366b2c780ffbf4aec83d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VNet.System.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VNet.System.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
